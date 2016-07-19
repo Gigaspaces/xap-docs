d24382e822393df54be30dbb5e35a264ec8382d3
croffler <chrisroffler@Chriss-MacBook-Pro.local>
2016-07-19 10:33:39 +0200

    adding popover to version buttons

---
+++ site/themes/hugo-bootswatch/static/js/enterprise-download.js
@@ -0,0 +1,67 @@
+function isScrolledIntoView(elem, offsetFromTop)
+{
+    offsetFromTop = typeof offsetFromTop !== 'undefined' ? offsetFromTop : 0;
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop() + offsetFromTop;
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(document).ready(function() {
+
+
+    $('#enterprise-download').click(function(e) {
+       location.href="http://www.gigaspaces.com/xap-download";
+    });
+
+    
+    var enterprisePopupTitle = 'XAP Enterprise';
+    var enterprisePopupText = 'The features below are not available in the open source edition of XAP. You may download a trial version of the Enterprise edition at the GigaSpaces Download Center.';
+
+    var mq = window.matchMedia( "(min-width: 1024px)" );
+    if (mq.matches) {
+        $("#enterprise-download").popover({
+            placement: 'left',
+            html: 'true',
+            title : '<span class="text-info"><strong>'+enterprisePopupTitle+'</strong></span>' +
+            '<button type="button" id="close" class="close" onclick="$(&quot;#enterprise-download&quot;).popover(&quot;hide&quot;);">&times;</button>',
+            content : enterprisePopupText
+        });
+
+        function enablePopoverOnMouseover() {
+            $('#enterprise-download').on('mouseover',
+            function () {
+                $('#enterprise-download').popover('show');
+            });
+
+            $('#enterprise-download').on('mouseleave',
+            function () {
+                $('#enterprise-download').popover('hide');
+            });
+        }
+
+        var enterprisePopupPresented = sessionStorage.getItem('enterprisePopupPresented');
+        if (enterprisePopupPresented == null) {
+                setTimeout(function() {
+                    if (isScrolledIntoView($("#enterprise-download"), 115)) {
+                        $('#enterprise-download').popover('show');
+                        sessionStorage.setItem('enterprisePopupPresented', 'true');
+
+                        setTimeout(function() {
+                            $('#enterprise-donwload').popover('hide');
+                            enablePopoverOnMouseover();
+                        }, 5000);
+                    }
+                }, 500);
+        } else {
+            enablePopoverOnMouseover();
+        }
+    }
+});
+
---
+++ site/themes/hugo-bootswatch/static/js/premium-download.js
@@ -0,0 +1,67 @@
+function isScrolledIntoView(elem, offsetFromTop)
+{
+    offsetFromTop = typeof offsetFromTop !== 'undefined' ? offsetFromTop : 0;
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop() + offsetFromTop;
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(document).ready(function() {
+
+
+    $('#premium-download').click(function(e) {
+       location.href="http://www.gigaspaces.com/xap-download";
+    });
+
+    
+    var premiumPopupTitle = 'XAP Premium';
+    var premiumPopupText = 'The features below are not available in the open source edition of XAP. You may download a trial version of the Premium edition at the GigaSpaces Download Center.';
+
+    var mq = window.matchMedia( "(min-width: 1024px)" );
+    if (mq.matches) {
+        $("#premium-download").popover({
+            placement: 'left',
+            html: 'true',
+            title : '<span class="text-info"><strong>'+premiumPopupTitle+'</strong></span>' +
+            '<button type="button" id="close" class="close" onclick="$(&quot;#premium-download&quot;).popover(&quot;hide&quot;);">&times;</button>',
+            content : premiumPopupText
+        });
+
+        function enablePopoverOnMouseover() {
+            $('#premium-download').on('mouseover',
+            function () {
+                $('#premium-download').popover('show');
+            });
+
+            $('#premium-download').on('mouseleave',
+            function () {
+                $('#premium-download').popover('hide');
+            });
+        }
+
+        var premiumPopupPresented = sessionStorage.getItem('premiumPopupPresented');
+        if (premiumPopupPresented == null) {
+                setTimeout(function() {
+                    if (isScrolledIntoView($("#premium-download"), 115)) {
+                        $('#premium-download').popover('show');
+                        sessionStorage.setItem('premiumPopupPresented', 'true');
+
+                        setTimeout(function() {
+                            $('#premium-donwload').popover('hide');
+                            enablePopoverOnMouseover();
+                        }, 5000);
+                    }
+                }, 500);
+        } else {
+            enablePopoverOnMouseover();
+        }
+    }
+});
+
