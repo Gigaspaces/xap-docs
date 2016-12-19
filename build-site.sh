00fe2d00c6893991949afadeb36da73985b4430a
Niv Ingberg <niv@gigaspaces.com>
2016-12-19 11:51:54 +0200

    Update build-site.sh

--- build-site.sh
+++ build-site.sh
@@ -3,4 +3,5 @@ set -e
 
 ./generate-navbar.sh .
 hugo/hugo -s ./site -d ../_site 
-cp -R site/public/xap100net/ site/public/xap/10.0/dev/dotnet/
+ls ../_site
+cp -R ../_site/xap100net/ ../_site/xap/10.0/dev/dotnet/
