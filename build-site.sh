a9fbb67748e4089320d8d657ecc1e477cd503e26
Niv Ingberg <niv@gigaspaces.com>
2016-12-19 20:20:45 +0200

    Disable output mapping

--- build-site.sh
+++ build-site.sh
@@ -3,4 +3,4 @@ set -e
 
 ./generate-navbar.sh .
 hugo/hugo -s ./site -d ../_site 
-mkdir -p _site/xap/10.0/dev/dotnet & cp -R _site/xap100net/* _site/xap/10.0/dev/dotnet
+# mkdir -p _site/xap/10.0/dev/dotnet & cp -R _site/xap100net/* _site/xap/10.0/dev/dotnet
