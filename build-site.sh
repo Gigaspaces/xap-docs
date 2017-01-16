55bb4c6ed050b987ae6c543048caa84c2e7461da
Niv Ingberg <niv@gigaspaces.com>
2017-01-16 10:25:43 +0200

    Revert to prev hugo

--- build-site.sh
+++ build-site.sh
@@ -2,5 +2,6 @@ set -x
 set -e
 
 ./generate-navbar.sh .
-hugo_0.17/hugo -s ./site -d ../_site 
+hugo/hugo -s ./site -d ../_site 
+#hugo_0.17/hugo -s ./site -d ../_site 
 # mkdir -p _site/xap/10.0/dev/dotnet & cp -R _site/xap100net/* _site/xap/10.0/dev/dotnet
