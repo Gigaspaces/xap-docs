78c597f0fa4296f17a5a358452f5fb2aca131c52
Niv Ingberg <niv@gigaspaces.com>
2017-01-16 10:20:52 +0200

    Updated hugo script to fetch hugo 0.17

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,8 +1,8 @@
 set -x
 set -e
-if [ ! -e hugo ]; then
-  wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz
-  tar xvzf hugo_0.14_linux_amd64.tar.gz
-  mv hugo_0.14_linux_amd64 hugo
-  mv hugo/hugo_0.14_linux_amd64 hugo/hugo
+if [ ! -e hugo_0.17 ]; then
+  wget https://github.com/spf13/hugo/releases/download/v0.17/hugo_0.17_Linux-64bit.tar.gz
+  tar xvzf hugo_0.17_Linux-64bit.tar.gz
+  mv hugo_0.17_linux_amd64 hugo_0.17
+  mv hugo_0.17/hugo_0.17_linux_amd64 hugo_0.17/hugo
 fi
