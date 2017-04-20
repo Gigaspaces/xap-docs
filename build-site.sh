da55e5474c4c8657ce3c3fdec6f6a38d6887b7a0
Niv Ingberg <niv@gigaspaces.com>
2017-04-20 14:33:48 +0300

    Copying output to alternative structure

--- build-site.sh
+++ build-site.sh
@@ -4,4 +4,5 @@ set -e
 ./generate-navbar.sh .
 #hugo/hugo -s ./site -d ../_site 
 hugo_0.17/hugo -s ./site -d ../_site 
-# mkdir -p _site/xap/10.0/dev/dotnet & cp -R _site/xap100net/* _site/xap/10.0/dev/dotnet
+mkdir -p _site/xap/10.1/dev/dotnet & cp -R _site/xap101net/* _site/xap/10.1/dev/dotnet
+mkdir -p _site/xap/10.1/net & cp -R _site/xap101net/* _site/xap/10.1/net
