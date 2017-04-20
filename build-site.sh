aafe73a243006d794a356754f023a0faa4bdec59
Niv Ingberg <niv@gigaspaces.com>
2017-04-20 16:23:44 +0300

    touch commit to trigger build

--- build-site.sh
+++ build-site.sh
@@ -4,5 +4,6 @@ set -e
 ./generate-navbar.sh .
 #hugo/hugo -s ./site -d ../_site 
 hugo_0.17/hugo -s ./site -d ../_site 
+
 mkdir -p _site/xap/10.1/dev/dotnet & cp -R _site/xap101net/* _site/xap/10.1/dev/dotnet
 mkdir -p _site/xap/10.1/net & cp -R _site/xap101net/* _site/xap/10.1/net
