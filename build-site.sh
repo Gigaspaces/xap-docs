203ba1dec921d246d1ade66f70d6fff1f8f8f0b2
Niv Ingberg <niv@gigaspaces.com>
2017-05-21 15:50:46 +0300

    Copying output to new structure

--- build-site.sh
+++ build-site.sh
@@ -5,5 +5,10 @@ set -e
 #hugo/hugo -s ./site -d ../_site 
 hugo_0.17/hugo -s ./site -d ../_site 
 
+# 10.1 redirects
+mkdir -p _site/xap/10.1/dev/java & cp -R _site/xap101/* _site/xap/10.1/dev/java
 mkdir -p _site/xap/10.1/dev/dotnet & cp -R _site/xap101net/* _site/xap/10.1/dev/dotnet
-mkdir -p _site/xap/10.1/net & cp -R _site/xap101net/* _site/xap/10.1/net
+mkdir -p _site/xap/10.1/tutorial/java & cp -R _site/xap101tut/* _site/xap/10.1/tutorial/java
+mkdir -p _site/xap/10.1/tutorial/dotnet & cp -R _site/xap101tutnet/* _site/xap/10.1/tutorial/dotnet
+mkdir -p _site/xap/10.1/admin & cp -R _site/xap101adm/* _site/xap/10.1/admin
+mkdir -p _site/xap/10.1/security & cp -R _site/xap101sec/* _site/xap/10.1/security
