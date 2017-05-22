77f283dbd52b0f36f644cab2cabe942017fb4be3
Niv Ingberg <niv@gigaspaces.com>
2017-05-22 16:02:09 +0300

    Added 10.2 redirects

--- build-site.sh
+++ build-site.sh
@@ -13,3 +13,12 @@ mv _site/xap101tut _site/xap/10.1/tut-java
 mv _site/xap101nettut _site/xap/10.1/tut-dotnet
 mv _site/xap101adm _site/xap/10.1/admin
 mv _site/xap101sec _site/xap/10.1/security
+
+# 10.2 redirects
+mkdir -p _site/xap/10.2
+mv _site/xap102 _site/xap/10.2/dev-java
+mv _site/xap102net _site/xap/10.2/dev-dotnet
+mv _site/xap102tut _site/xap/10.2/tut-java
+mv _site/xap102nettut _site/xap/10.2/tut-dotnet
+mv _site/xap102adm _site/xap/10.2/admin
+mv _site/xap102sec _site/xap/10.2/security
