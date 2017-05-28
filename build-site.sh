263f16bdf15b7f8d95ae1138a901577bbd3c66a9
Niv Ingberg <niv@gigaspaces.com>
2017-05-28 17:13:35 +0300

    Added 12.0, 12.1 redirects

--- build-site.sh
+++ build-site.sh
@@ -31,3 +31,21 @@ mv _site/xap110tut _site/xap/11.0/tut-java
 mv _site/xap110nettut _site/xap/11.0/tut-dotnet
 mv _site/xap110adm _site/xap/11.0/admin
 mv _site/xap110sec _site/xap/11.0/security
+
+# 12.0 redirects
+mkdir -p _site/xap/12.0
+mv _site/xap120 _site/xap/12.0/dev-java
+mv _site/xap120net _site/xap/12.0/dev-dotnet
+mv _site/xap120tut _site/xap/12.0/tut-java
+mv _site/xap120nettut _site/xap/12.0/tut-dotnet
+mv _site/xap120adm _site/xap/12.0/admin
+mv _site/xap120sec _site/xap/12.0/security
+
+# 12.1 redirects
+mkdir -p _site/xap/12.1
+mv _site/xap121 _site/xap/12.1/dev-java
+mv _site/xap121net _site/xap/12.1/dev-dotnet
+mv _site/xap121tut _site/xap/12.1/tut-java
+mv _site/xap121nettut _site/xap/12.1/tut-dotnet
+mv _site/xap121adm _site/xap/12.1/admin
+mv _site/xap121sec _site/xap/12.1/security
