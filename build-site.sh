00683fc06fc243c0aa8ab66cc94e0964c652d493
Niv Ingberg <niv@gigaspaces.com>
2017-05-28 22:58:27 +0300

    Restructure 10.0 urls

--- build-site.sh
+++ build-site.sh
@@ -5,6 +5,13 @@ set -e
 #hugo/hugo -s ./site -d ../_site 
 hugo_0.17/hugo -s ./site -d ../_site 
 
+# 10.0 redirects
+mkdir -p _site/xap/10.0
+mv _site/xap100 _site/xap/10.0/dev-java
+mv _site/xap100net _site/xap/10.0/dev-dotnet
+mv _site/xap100adm _site/xap/10.0/admin
+mv _site/xap100sec _site/xap/10.0/security
+
 # 10.1 redirects
 mkdir -p _site/xap/10.1
 mv _site/xap101 _site/xap/10.1/dev-java
