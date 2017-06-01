f029f6af8b3e581c868ae660d646699fec8f48d7
Niv Ingberg <niv@gigaspaces.com>
2017-06-01 10:18:51 +0300

    10.0 content restructure

--- build-site.sh
+++ build-site.sh
@@ -4,10 +4,3 @@ set -e
 ./generate-navbar.sh .
 #hugo/hugo -s ./site -d ../_site 
 hugo_0.17/hugo -s ./site -d ../_site 
-
-# 10.0 redirects
-mkdir -p _site/xap/10.0
-mv _site/xap100 _site/xap/10.0/dev-java
-mv _site/xap100net _site/xap/10.0/dev-dotnet
-mv _site/xap100adm _site/xap/10.0/admin
-mv _site/xap100sec _site/xap/10.0/security
