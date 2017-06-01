c269829457297874b80fb15ba84c5b080a67e263
Niv Ingberg <niv@gigaspaces.com>
2017-06-01 10:05:00 +0300

    12.0 content restructure

--- build-site.sh
+++ build-site.sh
@@ -12,15 +12,6 @@ mv _site/xap100net _site/xap/10.0/dev-dotnet
 mv _site/xap100adm _site/xap/10.0/admin
 mv _site/xap100sec _site/xap/10.0/security
 
-# 12.0 redirects
-mkdir -p _site/xap/12.0
-mv _site/xap120 _site/xap/12.0/dev-java
-mv _site/xap120net _site/xap/12.0/dev-dotnet
-mv _site/xap120tut _site/xap/12.0/tut-java
-mv _site/xap120nettut _site/xap/12.0/tut-dotnet
-mv _site/xap120adm _site/xap/12.0/admin
-mv _site/xap120sec _site/xap/12.0/security
-
 # 12.1 redirects
 mkdir -p _site/xap/12.1
 mv _site/xap121 _site/xap/12.1/dev-java
