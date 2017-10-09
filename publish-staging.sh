1672f592f8462c406993059829d84376c8d403c1
Niv Ingberg <niv@gigaspaces.com>
2017-10-09 20:49:55 +0300

    Experimenting with CI variables

--- publish-staging.sh
+++ publish-staging.sh
@@ -2,4 +2,9 @@
 
 set -x
 set -e
-echo Starting Staging CI...
+echo Publishing to staging (under construction)...
+echo S3_BUCKET_STAGING=$S3_BUCKET_STAGING
+echo S3_BUCKET_PRODUCTION=$S3_BUCKET_PRODUCTION
+export S3_BUCKET=$S3_BUCKET_STAGING
+echo S3_BUCKET=$S3_BUCKET
+echo Done
