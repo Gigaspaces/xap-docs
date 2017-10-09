d13e6cd2b61639d9af4e721d708a8571045573cf
Niv Ingberg <niv@gigaspaces.com>
2017-10-09 21:06:50 +0300

    Experimenting with CI
    Removed "set -x" to avoid verbose prints (later will include secrets)

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-set -x
 set -e
-echo "Publishing to staging (under construction)..."
-echo S3_BUCKET_STAGING=$S3_BUCKET_STAGING
-echo S3_BUCKET_PRODUCTION=$S3_BUCKET_PRODUCTION
 export S3_BUCKET=$S3_BUCKET_STAGING
-echo S3_BUCKET=$S3_BUCKET
-echo Done
+echo "Publishing staging to $S3_BUCKET (under construction)..."
