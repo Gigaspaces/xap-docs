7de596f6334fa891dbc0965d00bf3638f33fad17
Niv Ingberg <niv@gigaspaces.com>
2017-10-09 20:56:36 +0300

    experimenting with CI env vars

--- publish-staging.sh
+++ publish-staging.sh
@@ -2,7 +2,7 @@
 
 set -x
 set -e
-echo Publishing to staging (under construction)...
+echo "Publishing to staging (under construction)..."
 echo S3_BUCKET_STAGING=$S3_BUCKET_STAGING
 echo S3_BUCKET_PRODUCTION=$S3_BUCKET_PRODUCTION
 export S3_BUCKET=$S3_BUCKET_STAGING
