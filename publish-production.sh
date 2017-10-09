63dbaa9e4bf87cc7e5d2c96849f1e682e54057ad
Niv Ingberg <niv@gigaspaces.com>
2017-10-09 21:43:58 +0300

    Changes in CI
    - Modified circle.yml to publish production via script instead of inline
    - Added publish-production.sh
    - Modified s3_website.yml to accept bucket as env var and renamed vars for pluggability
    - Using publish-staging/production to trigger s3_website with different vars

--- publish-production.sh
+++ publish-production.sh
@@ -3,6 +3,6 @@
 set -e
 export S3_ID=$S3_ACCESS_KEY_ID
 export S3_SECRET=$S3_SECRET_KEY
-export S3_BUCKET=docs.gigaspaces.com-v2
-echo "Publishing staging to $S3_BUCKET..."
+export S3_BUCKET=docs.gigaspaces.com
+echo "Publishing production to $S3_BUCKET..."
 s3_website push
