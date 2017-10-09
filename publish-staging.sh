bf3778b58aa31b7471da8318d47722f296d8fe18
Niv Ingberg <niv@gigaspaces.com>
2017-10-09 21:53:02 +0300

    Updated staging credentials

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-export S3_ID=$S3_ACCESS_KEY_ID
-export S3_SECRET=$S3_SECRET_KEY
+export S3_ID=$STAGING_S3_ID
+export S3_SECRET=$STAGING_S3_SECRET
 export S3_BUCKET=docs.gigaspaces.com-v2
 echo "Publishing staging to $S3_BUCKET..."
 s3_website push
