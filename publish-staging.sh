aab9c8bdfd293f4945e64f6d1cea04b3301ad7f4
Niv Ingberg <niv@gigaspaces.com>
2017-10-10 08:33:53 +0300

    Updating staging CI
    - publishing to new staging bucket
    - publishing to experimental production (temporary, until transition is completed)

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
 set -e
+
 export S3_ID=$STAGING_S3_ID
 export S3_SECRET=$STAGING_S3_SECRET
-export S3_BUCKET=docs.gigaspaces.com-v2
+export S3_BUCKET=docs-staging.gigaspaces.com
 echo "Publishing staging to $S3_BUCKET..."
+s3_website push
+
+export S3_ID=$PROD_S3_ID
+export S3_SECRET=$PROD_S3_SECRET
+export S3_BUCKET=docs.gigaspaces.com-v2
+echo "Publishing experimental production to $S3_BUCKET..."
 s3_website push
