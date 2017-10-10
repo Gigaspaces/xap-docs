82d17c2ffcc89a9c5f94d997f061d57217d51ec7
Niv Ingberg <niv@gigaspaces.com>
2017-10-10 09:54:53 +0300

    Retrying staging

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-#export S3_ID=$STAGING_S3_ID
-#export S3_SECRET=$STAGING_S3_SECRET
-#export S3_BUCKET=docs-staging.gigaspaces.com
-#export S3_ENDPOINT=eu-west-1
-#echo "Publishing staging to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
-#s3_website push
+export S3_ID=$STAGING_S3_ID
+export S3_SECRET=$STAGING_S3_SECRET
+export S3_BUCKET=docs-staging.gigaspaces.com
+export S3_ENDPOINT=eu-west-1
+echo "Publishing staging to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
+s3_website push
 
 export S3_ID=$PROD_S3_ID
 export S3_SECRET=$PROD_S3_SECRET
