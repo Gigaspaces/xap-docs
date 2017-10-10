edfd4ff5d2a681ce52b8236b6a3ebe7abc50629f
Niv Ingberg <niv@gigaspaces.com>
2017-10-10 12:32:49 +0300

    Experimenting with ERB for different targets

--- publish-production.sh
+++ publish-production.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-export S3_ID=$S3_ACCESS_KEY_ID
-export S3_SECRET=$S3_SECRET_KEY
-export S3_BUCKET=docs.gigaspaces.com
-export S3_ENDPOINT=us-east-1
-echo "Publishing production to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
+echo "Publishing production..."
+export DOCS_TARGET=PROD
 s3_website push
