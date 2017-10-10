41c46ef632f567605b883ac9b0a751cec9f90c0d
Niv Ingberg <niv@gigaspaces.com>
2017-10-10 09:14:50 +0300

    Updated CI to support different s3 endpoints

--- publish-production.sh
+++ publish-production.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
 set -e
+
 export S3_ID=$S3_ACCESS_KEY_ID
 export S3_SECRET=$S3_SECRET_KEY
 export S3_BUCKET=docs.gigaspaces.com
-echo "Publishing production to $S3_BUCKET..."
+export S3_ENDPOINT=us-east-1
+echo "Publishing production to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
 s3_website push
--- publish-staging.sh
+++ publish-staging.sh
@@ -4,11 +4,13 @@ set -e
 export S3_ID=$STAGING_S3_ID
 export S3_SECRET=$STAGING_S3_SECRET
 export S3_BUCKET=docs-staging.gigaspaces.com
-echo "Publishing staging to $S3_BUCKET..."
+export S3_ENDPOINT=eu-west-1
+echo "Publishing staging to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
 s3_website push
 
 export S3_ID=$PROD_S3_ID
 export S3_SECRET=$PROD_S3_SECRET
 export S3_BUCKET=docs.gigaspaces.com-v2
-echo "Publishing experimental production to $S3_BUCKET..."
+export S3_ENDPOINT=us-east-1
+echo "Publishing experimental production to S3 bucket $S3_BUCKET at $S3_ENDPOINT..."
 s3_website push
