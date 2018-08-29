027ce70b38bf379b47a2893d4d299625d6ae8bbd
Shlomo Sagir <shlomo.s@tech-tav.com>
2018-08-29 17:32:17 +0300

    Removed script debug mode

--- flare-migration/migrate-flare.bat
+++ flare-migration/migrate-flare.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-set fOut="output\xap\12.3"
+set fOut=".\output\xap\12.3"
 echo Updating Flare content ...
-cscript migrate-flare.js //x //nologo %fOut%
+cscript migrate-flare.js //nologo %fOut%
 echo Copying files ...
 ROBOCOPY /e %fOut% "..\..\xap-docs-flare\Content"
 echo Done
