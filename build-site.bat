666401054584d5e2fb3f50aeb2746fe7f981c08b
Niv Ingberg <niv@gigaspaces.com>
2018-09-20 11:57:15 +0300

    Enhanced build script to publish to S3

--- build-site.bat
+++ build-site.bat
@@ -6,4 +6,10 @@ echo *** Building hugo site ***
 call build-site-hugo.bat
 echo *** Building flare site ***
 call build-site-flare.bat
+if defined Destination (
+  if %Destination%==staging (set DOCS_BUCKET=docs-staging.gigaspaces.com) else (set DOCS_BUCKET=docs.gigaspaces.com-v2)
+  echo Publishing site to %Destination% (%DOCS_BUCKET%)...
+  set AWS_PROFILE=%Destination%
+  aws s3 sync --delete --storage-class=REDUCED_REDUNDANCY output s3://%DOCS_BUCKET%
+)
 echo *** Site build completed ***
