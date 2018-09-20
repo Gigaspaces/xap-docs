88e876ccc89b27f135875635fbac28de81d82bc2
Niv Ingberg <niv@gigaspaces.com>
2018-09-20 16:52:09 +0300

    Moved static images from hugo to flare

--- run-hugo.bat
+++ run-hugo.bat
@@ -1,5 +1,5 @@
 @echo off
 call generate-navbar.bat
 set HUGO_HOME=%~dp0\bin\hugo-0.17
-rem SET HUGO_STATICDIR=../site-flare/Content/Resources/Static
+SET HUGO_STATICDIR=../site-flare/Content/Resources/Static
 %HUGO_HOME%\hugo -s site-hugo %*
