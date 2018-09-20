1c0f96721f499cf4f9b16333bd09b8394f6287e9
Niv Ingberg <niv@gigaspaces.com>
2018-09-20 17:06:08 +0300

    Fixed env var expansion

--- build-site.bat
+++ build-site.bat
@@ -8,8 +8,8 @@ echo *** Building flare site ***
 call build-site-flare.bat
 if defined Destination (
   if %Destination%==staging (set DOCS_BUCKET=docs-staging.gigaspaces.com) else (set DOCS_BUCKET=docs.gigaspaces.com-v2)
-  echo Publishing site to %Destination% (%DOCS_BUCKET%)
-  set AWS_PROFILE=%Destination%
-  aws s3 sync --delete --storage-class=REDUCED_REDUNDANCY output s3://%DOCS_BUCKET%
+  echo Publishing site to !Destination! (!DOCS_BUCKET!)
+  set AWS_PROFILE=!Destination!
+  aws s3 sync --delete --storage-class=REDUCED_REDUNDANCY output s3://!DOCS_BUCKET!
 )
 echo *** Site build completed ***
