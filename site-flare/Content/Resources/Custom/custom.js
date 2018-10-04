a872d2274a5bd81eefc777a5771d6dc7550474e0
Shlomo Sagir <shlomo.s@tech-tav.com>
2018-10-04 20:44:30 +0300

    Skin Fixes
    + Fixed Side Menu
    + Added Version Menu

--- site-flare/Content/Resources/Custom/custom.js
+++ site-flare/Content/Resources/Custom/custom.js
@@ -21,12 +21,18 @@
     ready: function () {
       $(function () {
         debug('Registering events');
-
         _self.events.scroll();
 
         debug('Calling methods');
+        var id = setInterval(function () {
+          if (!$('.sideContent .menu .selected').length) return;
+          clearInterval(id);
+          _self.methods.sidebar();
+        }, 500);
         _self.methods.breadcrumb();
-        _self.methods.sidebar();
+        console.log('MENU:: Start');
+        _self.methods.generateVersionMenu();
+        console.log('MENU:: End');
       });
     },
     methods: {
@@ -34,22 +40,73 @@
         $('.breadcrumbs').prependTo('.main-section');
       },
       sidebar: function () {
-        var id = setInterval(function () {
-          if (!$('.sideContent .menu .selected').length) return;
-          clearInterval(id);
-          $('.sideContent .menu .selected-child').removeClass('selected-child');
-          $('.sideContent .menu .selected').parent().addClass('selected-child');
-        }, 500);
+        $('.sideContent .selected-child-parent').removeClass('selected-child-parent');
+        $('.sideContent .selected-child-menu').removeClass('selected-child-menu');
+        $('.sideContent .selected-child').removeClass('selected-child');
+
+        $('.sideContent .selected').parent().addClass('selected-child');
+        $('.sideContent .selected-child').parents('ul:not(.menu)').addClass('selected-child-menu');
+        $('.sideContent .selected-child').parents('ul:not(.menu)').last().prev().addClass('selected-child-parent');
+      },
+      generateVersionMenu: function () {
+        _self.utils.getFile({
+          callback: _self.methods.buildVersionMenu,
+          filename: '/custom.menu.js'
+        });
+      },
+      buildVersionMenu: function () {
+        var mList;
+        var curr = location.href.match(/\/(\d+?[\.\d]*?)\//)[1];
+        var versionMenuStart = '<ul class="version-menu"><li><a href="#">' + curr + '<span class="caret"></span></a><ul>';
+        var versionMenuEnd = '</ul></li></ul>';
+
+        debug('buildVersionMenu::versionData:', versionData);
+        mList = versionMenuStart;
+        for (var v in versionData) {
+          if (v == curr)
+            mList += '<li><a href="#" onClick="return false;">' + v + '</a></li>';
+          else
+            mList += '<li><a href="' + versionData[v] + '">' + v + '</a></li>';
+        }
+        mList += versionMenuEnd;
+        debug('buildVersionMenu::mList:', mList);
+
+        $(mList).appendTo('.logo-wrapper');
+        $('.version-menu>li>a')
+          .click(function (e) {
+            e.preventDefault();
+            $('.version-menu>li').toggleClass('open');
+          }).blur(function (e) {
+            e.preventDefault();
+            $('.version-menu>li').removeClass('open');
+          });
       }
     },
     events: {
       scroll: function () {
-        _self.methods.sidebar();
+        $('.body-container').scroll(_self.methods.sidebar);
       }
     },
     utils: {
       getVar: function (vname) {
         return $(vname).text();
+      },
+      getFile: function (options) {
+        debug('getFile::protocol:', location.protocol);
+        if (location.protocol == 'file:') {
+          debug('getFile::protocol:Aborting get file on local files system');
+          return false;
+        }
+
+        options.filename = options.filename || '';
+        options.callback = options.callback || function () {};
+        debug('getFile::options:', options);
+
+        debug('getFile::filename:', options.filename);
+        $.getScript(options.filename)
+          .done(function () {
+            options.callback(options);
+          })
       }
     }
   };
---
+++ site-flare/Content/Resources/Custom/custom.menu.js
@@ -0,0 +1,13 @@
+var versionData = {
+  "14.0": "/xap/14.0/Default.html",
+  "12.3": "/xap/12.3",
+  "12.2": "/xap/12.2",
+  "12.1": "/xap121.html",
+  "12.0": "/xap120.html",
+  "11.0": "/xap110.html",
+  "10.2": "/xap102.html",
+  "10.1": "/xap101.html",
+  "10.0": "/xap100.html",
+  "9.7": "/xap97.html",
+  "Archive": "/archive.html"
+};
