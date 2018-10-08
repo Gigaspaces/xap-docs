53ec75fa80aa07b67af7aa14849b84a3cb75273c
Shlomo Sagir <shlomo.s@tech-tav.com>
2018-10-08 09:54:31 +0300

    Skin Fixes
    Fixes:
    - Adds Max content width
    - Adds Resources menu
    - Adds Nav bar buttone
    - Adds Back to top scroll
    - Adds Home + version to breadcrumbs

--- site-flare/Content/Resources/Custom/custom.js
+++ site-flare/Content/Resources/Custom/custom.js
@@ -4,7 +4,9 @@
       version: '1.0',
       prefix: 'TOPNAV:::',
       debug: true,
-      isIframe: window.self !== window.top
+      isIframe: window.self !== window.top,
+      prodVer: location.href.match(/\/(\d+?[\.\d]*?)\//)[1],
+      scrollToTopMin: 200
     },
     log: function () {
       if (!_self.props.debug) return;
@@ -30,14 +32,26 @@
           _self.methods.sidebar();
         }, 500);
         _self.methods.breadcrumb();
-        console.log('MENU:: Start');
-        _self.methods.generateVersionMenu();
-        console.log('MENU:: End');
+        _self.methods.scrollToTop();
+        _self.methods.generateNavItems();
       });
     },
     methods: {
       breadcrumb: function () {
+        var prodVerUrl = location.href.match(/(^.*?)\/(\d+?[\.\d]*?)\//)[1];
+        $('<a href="/"><i class="fa fa-home fa-lg"></i></a><span class="MCBreadcrumbsDivider"> &gt;&gt; </span><a href="' + prodVerUrl + '" class="MCBreadcrumbsLink">' + _self.props.prodVer + '</a><span class="MCBreadcrumbsDivider"> &gt;&gt; </span>').prependTo('.breadcrumbs');
         $('.breadcrumbs').prependTo('.main-section');
+      },
+      scrollToTop: function () {
+        $('.body-container').scroll(function () {
+          if ($('.body-container').scrollTop() >= _self.props.scrollToTopMin)
+            $('.scrollToTop').addClass('show');
+          else
+            $('.scrollToTop').removeClass('show');
+        });
+        $('.scrollToTop').click(function () {
+          $('.body-container').scrollTop(0);
+        });
       },
       sidebar: function () {
         $('.sideContent .selected-child-parent').removeClass('selected-child-parent');
@@ -48,38 +62,58 @@
         $('.sideContent .selected-child').parents('ul:not(.menu)').addClass('selected-child-menu');
         $('.sideContent .selected-child').parents('ul:not(.menu)').last().prev().addClass('selected-child-parent');
       },
-      generateVersionMenu: function () {
+      generateNavItems: function () {
         _self.utils.getFile({
-          callback: _self.methods.buildVersionMenu,
+          callback: _self.methods.buildNavItems,
           filename: '/custom.menu.js'
         });
       },
-      buildVersionMenu: function () {
-        var mList;
-        var curr = location.href.match(/\/(\d+?[\.\d]*?)\//)[1];
-        var versionMenuStart = '<ul class="version-menu"><li><a href="#">' + curr + '<span class="caret"></span></a><ul>';
-        var versionMenuEnd = '</ul></li></ul>';
+      buildNavItems: function () {
+        var vMenu = null,
+          rMenu = null;
+        var vLabel;
+        var menuStart = '<ul id="MENU_ID" class="nav-menu"><li><a href="#">MENU_LABEL<span class="caret"></span></a><ul>';
+        var menuEnd = '</ul></li></ul>';
 
-        debug('buildVersionMenu::versionData:', versionData);
-        mList = versionMenuStart;
+        vMenu = menuStart.replace('MENU_ID', 'version-menu');
         for (var v in versionData) {
-          if (v == curr)
-            mList += '<li><a href="#" onClick="return false;">' + v + '</a></li>';
+          vLabel = versionData[v].label || v;
+          if (v == _self.props.prodVer)
+            vMenu = vMenu.replace('MENU_LABEL', vLabel);
           else
-            mList += '<li><a href="' + versionData[v] + '">' + v + '</a></li>';
+            vMenu += '<li><a href="' + versionData[v].url + '">' + vLabel + '</a></li>';
         }
-        mList += versionMenuEnd;
-        debug('buildVersionMenu::mList:', mList);
-
-        $(mList).appendTo('.logo-wrapper');
-        $('.version-menu>li>a')
+        vMenu += menuEnd;
+        $(vMenu).appendTo('.logo-wrapper');
+        $('#version-menu>li>a')
           .click(function (e) {
             e.preventDefault();
-            $('.version-menu>li').toggleClass('open');
+            $('#version-menu>li').toggleClass('open');
           }).blur(function (e) {
             e.preventDefault();
-            $('.version-menu>li').removeClass('open');
+            $('#version-menu>li').removeClass('open');
           });
+
+        for (var r in resourcesData) {
+          if (!rMenu)
+            rMenu = menuStart.replace('MENU_ID', 'resources-menu').replace('MENU_LABEL', r);
+          else
+            rMenu += '<li><a href="' + resourcesData[r].url + '">' + r + '</a></li>';
+        }
+        rMenu += menuEnd;
+        $(rMenu).appendTo('.logo-wrapper');
+        $('#resources-menu>li>a')
+          .click(function (e) {
+            e.preventDefault();
+            $('#resources-menu>li').toggleClass('open');
+          }).blur(function (e) {
+            e.preventDefault();
+            $('#resources-menu>li').removeClass('open');
+          });
+
+        for (var i = 0; i < buttonsData.length; i++) {
+          $(buttonsData[i]).prependTo('.nav-search-wrapper');
+        }
       }
     },
     events: {
--- site-flare/Content/Resources/Custom/custom.menu.js
+++ site-flare/Content/Resources/Custom/custom.menu.js
@@ -1,13 +1,29 @@
 var versionData = {
-  "14.0": "/xap/14.0/Default.html",
-  "12.3": "/xap/12.3",
-  "12.2": "/xap/12.2",
-  "12.1": "/xap121.html",
-  "12.0": "/xap120.html",
-  "11.0": "/xap110.html",
-  "10.2": "/xap102.html",
-  "10.1": "/xap101.html",
-  "10.0": "/xap100.html",
-  "9.7": "/xap97.html",
-  "Archive": "/archive.html"
+  '14.0': { 'url': '/xap/14.0/Default.html', 'label': '14.0 Early Access' },
+  '12.3': { 'url': '/xap/12.3' },
+  '12.2': { 'url': '/xap/12.2' },
+  '12.1': { 'url': '/xap121.html' },
+  '12.0': { 'url': '/xap120.html' },
+  '11.0': { 'url': '/xap110.html' },
+  '10.2': { 'url': '/xap102.html' },
+  '10.1': { 'url': '/xap101.html' },
+  '10.0': { 'url': '/xap100.html' },
+  '9.7': { 'url': '/xap97.html' },
+  'Archive': { 'url': '/archive.html' }
 };
+
+var resourcesData = {
+  'Resources': { 'url': '#' },
+  'Early Access': { 'url': '/early_access' },
+  'Frequently Asked Questions': { 'url': '/faq' },
+  'API Docs': { 'url': '/api_documentation' },
+  'Videos': { 'url': '/videos' },
+  'Solutions & Patterns': { 'url': '/sbp' },
+  'Blog': { 'url': 'http://blog.gigaspaces.com', 'target': '_blank' },
+  'Forum': { 'url': 'http://ask.gigaspaces.org', 'target': '_blank' }
+};
+
+var buttonsData = [
+  '<button type="button" onclick="location.href=\'https://github.com/xap\'" class="navbar-btn btn-fork"> <i class="fa fa-github" aria-hidden="true"></i> Fork me on Github </button>',
+  '<button type="button" onclick="location.href=\'http://www.gigaspaces.com/xap-download\'" class="navbar-btn btn-download">Get Latest GA </button>'
+]
