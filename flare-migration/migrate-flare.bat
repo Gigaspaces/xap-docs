92fc6fd3458e60284da1933d7fe4ac6a8fc4ab44
Shlomo Sagir <shlomo.s@tech-tav.com>
2018-12-20 10:34:41 +0200

    Added comments for Penina

--- flare-migration/migrate-flare.bat
+++ flare-migration/migrate-flare.bat
@@ -1,8 +1,10 @@
 @echo off
 setlocal
+REM fOut is the path to the content to migrate
 set fOut=".\output\xap\14.0"
 echo Updating Flare content ...
 cscript migrate-flare.js //nologo %fOut%
 echo Copying files ...
+REM second parameter is the path where to store it in Flare
 ROBOCOPY /e %fOut% "..\site-flare\Content"
 echo Done
