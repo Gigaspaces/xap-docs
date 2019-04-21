f9e92f7418aa512279002dd496b5acc58f18599c
Niv Ingberg <niv@gigaspaces.com>
2019-04-21 14:42:37 +0300

    Enhanced migration to support sbp

--- flare-migration/migrate.bat
+++ flare-migration/migrate.bat
@@ -4,4 +4,5 @@ call migrate-import.bat
 call migrate-override.bat
 call migrate-sidenav.bat
 call migrate-hugo.bat
-call migrate-flare.bat
+REM call migrate-flare.bat xap\12.3
+call migrate-flare.bat sbp sbp
