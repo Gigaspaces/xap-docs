e8d8e816d1de961de1a2077fced554980e750204
Rivka Teller <rivka.t@tech-tav.com>
2019-09-02 11:57:51 +0300

    Decision tree cards, various style changes

--- site-flare/Content/Resources/Custom/custom.js
+++ site-flare/Content/Resources/Custom/custom.js
@@ -224,7 +224,7 @@
           .click(function (e) {
             e.preventDefault();
             $('.nav-search-wrapper').show();
-            $('.search-field.needs-pie').attr('placeholder','Search for anything...');
+            $('.search-field.needs-pie').attr('placeholder','Search...');
             $('#search-close').show();
             $(this).hide();
           });
