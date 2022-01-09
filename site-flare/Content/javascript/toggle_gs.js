4e3c54279c0eb5c7f02475d432d717f722d3d27e
David Freedman <david.freedman@gigaspaces.com>
2022-01-09 17:16:20 +0200

    gs

---
+++ site-flare/Content/javascript/toggle_gs.js
@@ -0,0 +1,66 @@
+function toggleGS(divNum)
+{
+
+	
+	switch (divNum) {
+	case 'none':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '1':
+	document.getElementById('1').style.display = 'block';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '2':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'block';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '3':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'block';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '4':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'block';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '5':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'block';
+	document.getElementById('6').style.display = 'none';
+		break;
+		case '6':
+	document.getElementById('1').style.display = 'none';
+	document.getElementById('2').style.display = 'none';
+	document.getElementById('3').style.display = 'none';
+	document.getElementById('4').style.display = 'none';
+	document.getElementById('5').style.display = 'none';
+	document.getElementById('6').style.display = 'block';
+		break;
+		
+	}
+		
+
+}
