369c625c269b24e9d420368d4b9f522a5807a517
David Freedman <david.freedman@gigaspaces.com>
2022-09-19 11:21:19 +0300

    misc

--- site-flare/Content/Resources/Custom/page-toc.js
+++ site-flare/Content/Resources/Custom/page-toc.js
@@ -1,41 +1,40 @@
 ﻿/* based on script from */
 /* https://css-tricks.com/automatic-table-of-contents */
-
 $(document).ready(function() {
+		
+	 
+		//		console.log("Hello world!..bbb.");
 	 
 	 
-//		console.log("Hello world!..bbb.");
-	 
-	 
-/*
-		 style='list-style-type:square  !important;'
+		/*
+		style='list-style-type:square  !important;'
 		
 	 
 		/* check if h2 or h3 exists on page. Only count visible headings */
 		
-//		console.log("11111");
+		//		console.log("11111");
 
 		
-//		if (f($("#" + name).length == 0)  {
+		//		if (f($("#" + name).length == 0)  {
 		if ($("#dfb_toc_here").length != 0) {
-//		var element_toc_marker = document.getElementById("dfb_toc");
-//
-//		if(typeof(element_toc_marker) != 'undefined' && element != null){
+			//		var element_toc_marker = document.getElementById("dfb_toc");
+			//
+			//		if(typeof(element_toc_marker) != 'undefined' && element != null){
 			
-//			$("#dfb_toc_block_none").insertAfter("#dfb_toc_here");
+			//			$("#dfb_toc_block_none").insertAfter("#dfb_toc_here");
 			$("#page_toc").insertAfter("#dfb_toc_here");
 			
-	//		console.log("22222");
+			//		console.log("22222");
 
          
 			/* build the opening tags of the structure */
 			var ToC =
 				"<nav  style='list-style-type:none !important;'>" +
 				"<ul  >";
-/*			var ToC =
-				"<nav role='navigation' class='table-of-contents'>" +
-				"<ul  style='list-style-type:none;' >";
-*/			
+			/*			var ToC =
+			"<nav role='navigation' class='table-of-contents'>" +
+			"<ul  style='list-style-type:none;' >";
+			*/			
       
 			/* set up variables used */
 			var newLine, el, title, link, tag;
@@ -45,26 +44,26 @@ $(document).ready(function() {
 			var linkCount = 0;
 			var H1Count = 0;
 			
-	//		console.log("33333");
+			//		console.log("33333");
 
 			
          
 			/* find all h2 and h3 --- and h4 --- and h1 ---- on page. Only count visible headings */
 			$("h1:visible,h2:visible,h3:visible,h4:visible").each(function() {
          
-			//		console.log("44444");
+					//		console.log("44444");
 					/* get the heading */
-					el = $(this);
-			//		console.log("el is " + el);
-		//			console.log("55555");
+					el = (this);
+					//		console.log("el is " + el);
+					//			console.log("55555");
 					/* get the heading title */
 					title = el.text();
 					element_class = el.attr("class");
 					if ((element_class == "tc-pagetitle") || (element_class == "tc-pagetitle2"))
-						{skip_title = true}
+					{skip_title = true}
 					else
-			        	{skip_title = false}
-			//		 console.log("class >>>>" + element_class + "<<<<<"); 
+					{skip_title = false}
+					//		 console.log("class >>>>" + element_class + "<<<<<"); 
 					/* get the heading tag, this is capitalised, i.e. 'H2' or 'H3' or 'H1'*/
 					tag = el.prop("tagName");
       
@@ -81,7 +80,7 @@ $(document).ready(function() {
 
 					
 					/*					if ((tag == 'H1') && (H1Count != 1))  */
-//					if ((tag == 'H1') && (element_class != "tc-pagetitle") && (element_class != "tc-pagetitle2"))
+					//					if ((tag == 'H1') && (element_class != "tc-pagetitle") && (element_class != "tc-pagetitle2"))
 					if ((tag == 'H1') && (!skip_title))
 					{						
 
@@ -98,7 +97,7 @@ $(document).ready(function() {
 					
 					if (tag == 'H2')
 					{						
-					//	console.log("h2 has " + title);
+						//	console.log("h2 has " + title);
 						newLine =
 							"<ul   ><li class=" +
 							tag +
@@ -112,7 +111,7 @@ $(document).ready(function() {
 					if (tag == 'H3')
 					{						
 					
-					//	console.log(title);
+						//	console.log(title);
 						newLine =
 							"<ul  style='list-style-type:square;'   ><ul><li class=" +
 							tag +
@@ -125,7 +124,7 @@ $(document).ready(function() {
 					
 					if (tag == 'H4')
 					{						
-					//	console.log("h4 has " + title);
+						//	console.log("h4 has " + title);
 					
 						newLine =
 							"<ul> <ul><ul><li class=" +
@@ -141,11 +140,11 @@ $(document).ready(function() {
          
 					/* add the list item to the list */
 					/*			if ((tag == 'H1') && (H1Count == 1))  */
-//					if ((element_class != "tc-pagetitle") && (element_class != "tc-pagetitle2"))
+					//					if ((element_class != "tc-pagetitle") && (element_class != "tc-pagetitle2"))
 					if (!skip_title)
 					{ToC += newLine;}
 
-					});   
+				});   
          
 			/* Add closing tags to list */   
 			ToC +=
@@ -155,8 +154,9 @@ $(document).ready(function() {
 			
 			
 			/* Insert list in topic, and make visible */
-//			$("#page-toc").append(ToC).css("display", "block");
-			$("#page-toc-body").append(ToC).css("display", "block");
+			//			$("#page-toc").append(ToC).css("display", "block");
+			$("#page-toc-body").append(ToC);
+//			$("#page-toc-body").append(ToC).css("display", "none");
          
 		}
    
