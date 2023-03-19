b35a6deac5462e24531d00e9a112538e3aeb5ad8
TemimaG <temima.greenberg@gigaspaces.com>
2023-03-19 16:01:24 +0200

    changes for 16.2.1 showing 16.1 versions

--- site-flare/Content/Resources/Static/custom.menu.js
+++ site-flare/Content/Resources/Static/custom.menu.js
@@ -13,7 +13,7 @@
 	'latest': { 'url': '/latest/landing.html', 'label': '16.2.1' },	// also current
 	
 	'16.2': { 'url': '/16.2/landing.html', 'label': '16.2',  'topicBanner': 'old' },
-	'16.1.1': { 'url': '/16.1/landing.html', 'label': '16.1.1',  'topicBanner': 'old' },
+	'16.1.1': { 'url': '/16.1.1/landing.html', 'label': '16.1.1',  'topicBanner': 'old' },
 	'16.0': { 'url': '/16.0/landing.html', 'label': '16.0',  'topicBanner': 'old' },
 	'15.8': { 'url': '/15.8/landing.html', 'label': '15.8',  'topicBanner': 'old' },
 	'15.5': { 'url': '/15.5/landing.html', 'label': '15.5',  'topicBanner': 'old' },
